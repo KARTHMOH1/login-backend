464243282284a0f1112b54805c79565b5bc0f1b3
KARTHMOH1 <mohankarthik95@gmail.com>
2025-03-30 13:11:14 +0530

    Updated Backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require ("express")
+const app = express()
+const cors = require ("cors")
+app.use(cors({ origin: "https://your-vercel-app.vercel.app" })); // Replace with your Vercel URL
+
+
+var uname="karthik"
+var pass=123
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+app.post("/login",function(req,res){
+
+    console.log(req.body)
+
+    const username=req.body.username
+    const password=req.body.password
+
+    if(uname===username && pass==password){
+        res.send(true)
+        console.log("Login Successfull")
+    }else{
+        res.send(false)
+        console.log("Login UnSuccessfull")
+    }
+
+})
+
+app.listen(3000,function(){
+    console.log("Server Started............................")
+})
