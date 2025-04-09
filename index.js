be075bc2207561a1459aff6389c2ef51ac5cfcc2
KARTHMOH1 <mohankarthik95@gmail.com>
2025-04-09 19:55:07 +0530

    Updated Backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require ("express")
+const app = express()
+const PORT = process.env.PORT || 3000 
+const cors = require ("cors")
+app.use(cors());
+
+var uname="karthik"
+var pass=123
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+app.post("/login",function(req,res){
+
+    console.log(req.body)
+
+    const username=req.body.username
+    const password=req.body.password
+
+    if(uname===username && pass==password){
+        res.send(true)
+        console.log("Login Successfull")
+    }else{
+        res.send(false)
+        console.log("Login UnSuccessfull")
+    }
+
+})
+
+app.listen(PORT,function(){
+    console.log("Server Started............................")
+})
